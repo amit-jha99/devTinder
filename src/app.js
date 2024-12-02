5896740236d7ba1006fc71dad3f27eeb4eb9724f
amit-jha99 <amitjha121299@gmail.com>
2024-12-03 00:22:36 +0530

    Writing-Auth-Middleware

--- src/app.js
+++ src/app.js
@@ -1,33 +1,54 @@
 const express = require("express");
 
+
 const app = express();
+const {adminAuth,userAuth} = require("./middlewares/auth");
+//Handle Auth Middleware for all GET ,POST ...
+app.use("/admin",adminAuth);
+// app.use("/user",userAuth);
 
-//This will only handle GET call to /user
-// app.get("/user/:userId/:name/:password",(req,res)=>{
-//     // console.log(req.query);
-//     console.log(req.params);
-//     res.send({
-//         "Firstname" :"Amit",
-//         "Lastname" :"Jha",
-//         "Aura" :"Founder and CEO,CTO at Krishit&Party"
-//     });
-// })
-
-app.use("/user",(req,res,next)=>{
-    console.log("Handling route 1!!")
-    next();
-    // res.send("Response 1");
-},(req,res,next)=>{
-    console.log("Handling route 2!!")
-    // res.send("Response 2");
-    next();
-},(req,res,next)=>{
-    console.log("Handling route 3!!")
-    res.send("Response 3");
-},(req,res,next)=>{
-    console.log("Handling route 4!!")
-    res.send("Response 4");
+app.post("/user/login",()=>{
+    res.send("User logged in successfully");
 })
+
+app.get("/user",userAuth,(req,res)=>{
+    res.send("User Data Sent");
+
+})
+
+
+
+app.get("/admin/getAllData",(req,res)=>{
+    res.send("All Data Sent");
+})
+
+app.get("/admin/deleteUser",(req,res)=>{
+    res.send("Deleted a user");
+})
+
+// // GET /users => It checks all the app.xxx("matching route") function
+
+// //This will only handle GET call to /user
+// // app.get("/user/:userId/:name/:password",(req,res)=>{
+// //     // console.log(req.query);
+// //     console.log(req.params);
+// //     res.send({
+// //         "Firstname" :"Amit",
+// //         "Lastname" :"Jha",
+// //         "Aura" :"Founder and CEO,CTO at Krishit&Party"
+// //     });
+// // })
+
+// app.use("/user",(req,res,next)=>{
+//     console.log("Handling route 1!!")
+//     next();
+//     // res.send("Response 1");
+// });
+
+// app.use("/user",(req,res,next)=>{
+//     console.log("Handling the route user!!");
+//     res.send("Response 2");
+// });
 
 
 
