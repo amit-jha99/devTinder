00bf729799032ebd008fa05493462caf7c84e02c
amit-jha99 <amitjha121299@gmail.com>
2024-12-06 12:21:30 +0530

    created-dummy-signup -API

--- src/app.js
+++ src/app.js
@@ -1,59 +1,38 @@
 const express = require("express");
-
-
+const connectDB = require("./config/database");
 const app = express();
-const {adminAuth,userAuth} = require("./middlewares/auth");
-//Handle Auth Middleware for all GET ,POST ...
-app.use("/admin",adminAuth);
-// app.use("/user",userAuth);
+const User = require("./models/user");
 
-app.post("/user/login",()=>{
-    res.send("User logged in successfully");
+
+app.post("/signup",async(req,res)=>{
+  const userObj = {
+    firstName : "M.S.",
+    lastName : "Dhoni",
+    emailId : "Dhoni07@gmail.com",
+    password : "Mahi"
+  }
+
+  //Creating a new instance of the User model
+  const user = new User(userObj);
+  try{
+    await user.save();//This function returns you a  promise infact most of the mongoose function return you a promise
+    res.send("User Added succesfully!");
+
+  }catch(err){
+    res.status(400).send("Error saving the user:" + err.message);
+  }
+
+  
 })
 
-app.get("/user",userAuth,(req,res)=>{
-    res.send("User Data Sent");
+connectDB()
+  .then(() => {
+    console.log("Database connection established.....");
 
-})
-
-
-
-app.get("/admin/getAllData",(req,res)=>{
-    res.send("All Data Sent");
-})
-
-app.get("/admin/deleteUser",(req,res)=>{
-    res.send("Deleted a user");
-})
-
-// // GET /users => It checks all the app.xxx("matching route") function
-
-// //This will only handle GET call to /user
-// // app.get("/user/:userId/:name/:password",(req,res)=>{
-// //     // console.log(req.query);
-// //     console.log(req.params);
-// //     res.send({
-// //         "Firstname" :"Amit",
-// //         "Lastname" :"Jha",
-// //         "Aura" :"Founder and CEO,CTO at Krishit&Party"
-// //     });
-// // })
-
-// app.use("/user",(req,res,next)=>{
-//     console.log("Handling route 1!!")
-//     next();
-//     // res.send("Response 1");
-// });
-
-// app.use("/user",(req,res,next)=>{
-//     console.log("Handling the route user!!");
-//     res.send("Response 2");
-// });
-
-
-
-
-
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-});
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected!!!");
+  });
