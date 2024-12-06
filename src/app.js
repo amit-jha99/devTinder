4a580bf4182ed05f15300c1beb3c100e40b374a3
amit-jha99 <amitjha121299@gmail.com>
2024-12-06 13:21:12 +0530

    dynamic data in signup API

--- src/app.js
+++ src/app.js
@@ -2,18 +2,21 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+app.use(express.json());
 
 
 app.post("/signup",async(req,res)=>{
-  const userObj = {
-    firstName : "M.S.",
-    lastName : "Dhoni",
-    emailId : "Dhoni07@gmail.com",
-    password : "Mahi"
-  }
+
+  console.log(req.body);
+  // const userObj = {
+  //   firstName : "M.S.",
+  //   lastName : "Dhoni",
+  //   emailId : "Dhoni07@gmail.com",
+  //   password : "Mahi"
+  // }
 
   //Creating a new instance of the User model
-  const user = new User(userObj);
+  const user = new User(req.body);
   try{
     await user.save();//This function returns you a  promise infact most of the mongoose function return you a promise
     res.send("User Added succesfully!");
@@ -23,7 +26,7 @@ app.post("/signup",async(req,res)=>{
   }
 
   
-})
+});
 
 connectDB()
   .then(() => {
