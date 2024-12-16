8cea36c3a0f5142a9e70f061e3ec15518e0f5558
amit-jha99 <amitjha121299@gmail.com>
2024-12-17 00:38:39 +0530

    delete_API

--- src/app.js
+++ src/app.js
@@ -8,12 +8,7 @@ app.use(express.json());
 app.post("/signup",async(req,res)=>{
 
   console.log(req.body);
-  // const userObj = {
-  //   firstName : "M.S.",
-  //   lastName : "Dhoni",
-  //   emailId : "Dhoni07@gmail.com",
-  //   password : "Mahi"
-  // }
+  
 
   //Creating a new instance of the User model
   const user = new User(req.body);
@@ -27,6 +22,50 @@ app.post("/signup",async(req,res)=>{
 
   
 });
+
+
+//Get user by email
+app.get("/user",async (req,res)=>{
+  const userEmail = req.body.emailId;
+
+  try{
+    const user = await User.findOne({emailId: userEmail});
+    if(!user){
+      res.status(404).send("User not found!!");
+    }
+    res.send(user);
+    // const users = await User.find({emailId: userEmail});
+    // if(users.length === 0){
+    //   res.status(404).send("User not found !!");
+    // }
+    // res.send(users);
+
+  }
+  catch(err){
+    res.status(400).send("Something went wrong!!!")
+  }
+})
+//Feed API - GET/feed - get all the users from the database
+app.get("/feed",async (req,res)=>{
+  try{
+      const users = await  User.find({});
+      res.send(users);
+  }catch(err){
+    res.status(400).send("Something went wrong ");
+  }
+
+})
+
+app.delete("/user",async (req,res)=>{
+  const userId = req.body.userId;
+  try{
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted sucessfully!!");
+  }catch(err){
+    res.status(400).send("Something went wrong!!");
+  }
+})
+
 
 connectDB()
   .then(() => {
