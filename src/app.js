9fd7287796b96b472f6255644fe5126850c04f90
amit-jha99 <amitjha121299@gmail.com>
2025-01-03 19:55:51 +0530

    Refactor signup and user APIs for improved readability and error handling; implement allowed updates validation in user patch API

--- src/app.js
+++ src/app.js
@@ -4,33 +4,26 @@ const app = express();
 const User = require("./models/user");
 app.use(express.json());
 
-
-app.post("/signup",async(req,res)=>{
-
+app.post("/signup", async (req, res) => {
   console.log(req.body);
-  
 
   //Creating a new instance of the User model
   const user = new User(req.body);
-  try{
-    await user.save();//This function returns you a  promise infact most of the mongoose function return you a promise
+  try {
+    await user.save(); //This function returns you a  promise infact most of the mongoose function return you a promise
     res.send("User Added succesfully!");
-
-  }catch(err){
+  } catch (err) {
     res.status(400).send("Error saving the user:" + err.message);
   }
-
-  
 });
 
-
 //Get user by email
-app.get("/user",async (req,res)=>{
+app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
 
-  try{
-    const user = await User.findOne({emailId: userEmail});
-    if(!user){
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
       res.status(404).send("User not found!!");
     }
     res.send(user);
@@ -39,49 +32,61 @@ app.get("/user",async (req,res)=>{
     //   res.status(404).send("User not found !!");
     // }
     // res.send(users);
-
+  } catch (err) {
+    res.status(400).send("Something went wrong!!!");
   }
-  catch(err){
-    res.status(400).send("Something went wrong!!!")
-  }
-})
+});
 //Feed API - GET/feed - get all the users from the database
-app.get("/feed",async (req,res)=>{
-  try{
-      const users = await  User.find({});
-      res.send(users);
-  }catch(err){
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
     res.status(400).send("Something went wrong ");
   }
+});
 
-})
-
-app.delete("/user",async (req,res)=>{
+app.delete("/user", async (req, res) => {
   const userId = req.body.userId;
-  try{
+  try {
     const user = await User.findByIdAndDelete(userId);
     res.send("User deleted sucessfully!!");
-  }catch(err){
+  } catch (err) {
     res.status(400).send("Something went wrong!!");
   }
-})
+});
 
 //Update data of the user
 
-
-
-app.patch("/user", async (req, res) => {
-  const { userId, ...data } = req.body;
-  // const userId = req.body.userId;
-  // const data = req.body
+app.patch("/user/:userId", async (req, res) => {
+  // const { userId, ...data } = req.body;
+  const userId = req.params?.userId;
+  const data = req.body
 
   if (!userId) {
     return res.status(400).send("User ID is required");
   }
 
   try {
-    const updatedUser = await User.findByIdAndUpdate({_id:userId}, data, { new: true, runValidators: true });
-   
+    const ALLOWED_UPDATES = [
+      
+      "photoUrl",
+      "about",
+      "gender",
+      "skills",
+      "age",
+    ];
+
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Invalid updates!");
+    }
+    const updatedUser = await User.findByIdAndUpdate({ _id: userId }, data, {
+      new: true,
+      runValidators: true,
+    });
 
     if (!updatedUser) {
       return res.status(404).send("User not found");
@@ -91,10 +96,9 @@ app.patch("/user", async (req, res) => {
     res.status(200).send("User updated successfully");
   } catch (err) {
     console.error(err);
-    res.status(500).send("Updation failed" + " "+err.message);
+    res.status(500).send("Updation failed" + " " + err.message);
   }
 });
-
 
 connectDB()
   .then(() => {
