289da87b26365bf2c8b0f5292f1e7a08408e3164
amit-jha99 <amitjha121299@gmail.com>
2025-04-27 02:10:36 +0530

    Fix typo in bcrypt import statement in user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bycrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
