f06d453dbdb1428bfd7254d19625a39c7d9e9e60
amit-jha99 <amitjha121299@gmail.com>
2025-04-26 18:39:18 +0530

    Refactor auth middleware to remove adminAuth and streamline user authentication process

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,30 +1,27 @@
-const adminAuth  = (req,res,next)=>{
-    console.log("Admin auth is getting checked!!!");
-    const token = "xyz";
-    const isAdminAuthorized = token === "xyz";
-    if(!isAdminAuthorized){
-        res.status(401).send("Unauthorized request");
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel"); 
+const userAuth = async (req, res, next) => {
+  // Read the token from the request cookies
+  try {
+    const { token } = req.cookies;
+     //validate the token
+    const decodedObj = await jwt.verify(token, "AMIT@777$99");
+    const { _id } = decodedObj; 
 
+     //Find the username
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(401).send("User not found!!");
     }
-    else {
-        next();
-    }
-}
-const userAuth  = (req,res,next)=>{
-    console.log("Admin auth is getting checked!!!");
-    const token = "xyz";
-    const isAdminAuthorized = token === "xyz";
-    if(!isAdminAuthorized){
-        res.status(401).send("Unauthorized request");
-
-    }
-    else {
-        next();
-    }
-}
-
+    next(); // Call the next middleware or route handler
+   
+   
+  } catch (err) {
+    return res.status(400).send("Error" + err.message);
+   
+  }
+};
 
 module.exports = {
-    adminAuth,
-    userAuth
-}
+  userAuth,
+};
