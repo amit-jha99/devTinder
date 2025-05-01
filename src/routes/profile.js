87f9877146fb5f9ab477864199a8dcd1ba04927c
amit-jha99 <amitjha121299@gmail.com>
2025-05-02 01:46:27 +0530

    Update profile route to include '/view' for better clarity in endpoint structure

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,7 +2,7 @@ const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
     try {
       const user = req.user; //This will give you the user object from the middleware
   
