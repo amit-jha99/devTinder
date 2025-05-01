4725d4da7b18b40b93b8042835a673c24a67a67a
amit-jha99 <amitjha121299@gmail.com>
2025-05-02 02:27:25 +0530

    Refactor validation functions for improved readability and structure

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,19 +1,34 @@
 const validator = require("validator");
 const validateSignUpData = (req) => {
-       const {firstName,lastName,emailId,password} = req.body;
+  const { firstName, lastName, emailId, password } = req.body;
 
-       if(!firstName || !lastName){
-        throw new Error("First name and last name are required fields");
-       }
-       else if (!validator.isEmail(emailId)){
-        throw new Error("Email is not valid:" + emailId);
-       }
-       else if (!validator.isStrongPassword(password)){
-        throw new Error("Password is not strong enough:" + password);
-       }
-       
-}
+  if (!firstName || !lastName) {
+    throw new Error("First name and last name are required fields");
+  } else if (!validator.isEmail(emailId)) {
+    throw new Error("Email is not valid:" + emailId);
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("Password is not strong enough:" + password);
+  }
+};
 
+const validateEditProfileData = (req) => {
+  const allowedEditFields = [
+    "firstName",
+    "lastName",
+    "emilId",
+    "age",
+    "gender",
+    "photoUrl",
+    "about",
+    "skills",
+  ];
+  const isEditAllowed = Object.keys(req.body).every((field) =>
+    allowedEditFields.includes(field)
+  ); //This will return true if all the fields are valid
+  return isEditAllowed; //This will return true if all the fields are valid
+
+};
 module.exports = {
-    validateSignUpData,
-}
+  validateSignUpData,
+  validateEditProfileData,
+};
