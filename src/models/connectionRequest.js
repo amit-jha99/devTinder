57e5e79dcb97599c0ad747cdd5121e0ccc52a7ed
amit-jha99 <amitjha121299@gmail.com>
2025-05-03 11:07:50 +0530

    Refactor connectionRequest schema for improved status field validation

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -1,16 +1,21 @@
-const mongoose = require('mongoose');
-const connectionRequestSchema = new mongoose.Schema({
-    fromUserId:{
-        type:mongoose.Schema.Types.ObjectId
+const mongoose = require("mongoose");
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
     },
-    toUserId:{
-        type:mongoose.Schema.Types.ObjectId
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
     },
-    status:{
-        type:String,
-        enum:{
-            value:["ignore","interested","accepted","rejected"],
-            message:'{VALUE} is incorrect status type'
-        }
-    }
-})
+    status: {
+      type: String,
+      enum: {
+        value: ["ignore", "interested", "accepted", "rejected"],
+        message: "{VALUE} is incorrect status type",
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
