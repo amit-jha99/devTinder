ce4dc1d27b50d2ced43e773cebd4afdea8038aea
amit-jha99 <amitjha121299@gmail.com>
2025-05-03 11:29:48 +0530

    Refactor connection request route to handle sending requests with user IDs

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const requestRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
 
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
-    const user = req.user;
-    console.log("Connection request sent!!");
-    res.send(
-      user.firstName + " " + user.lastName + " sent you a connection request!!"
-    );
+requestRouter.post("/request/send/interested/:toUserId", userAuth, async (req, res) => {
+    
+   try {
+      const fromUserId = req.user._id; //This will give you the user object from the middleware
+      const toUserId = req.params.toUserId; //This will give you the user object from the middleware
+      const status = req.params.toUserId; //This will give you the user object from the middleware
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status
+      });
+      const data = await connectionRequest.save(); //This will save the updated user object in the database
+      res.json({
+        message: `${req.user.firstName} ${req.user.lastName} sent you a connection request!!`,
+        data: data,
+      })
+   }
+   catch(err) {
+        res.status(400).send("Something went wrong!!" + err.message);
+   }
+    
   });
   
 module.exports = requestRouter;
