46a7068977ea309e22c7d46b928c6aea0f25331f
amit-jha99 <amitjha121299@gmail.com>
2025-05-03 12:50:32 +0530

    Refactor request route for improved readability and structure

--- src/routes/request.js
+++ src/routes/request.js
@@ -3,9 +3,11 @@ const requestRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 
-requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
-    
-   try {
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
       const fromUserId = req.user._id; //This will give you the user object from the middleware
       const toUserId = req.params.toUserId; //This will give you the user object from the middleware
       const status = req.params.status; //This will give you the user object from the middleware
@@ -13,24 +15,43 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)
       const allowedStatus = ["ignored", "interested"];
       if (!allowedStatus.includes(status)) {
         return res.status(400).json({
-          message: "Invalid status type!!" + status
-        }); 
-      } 
+          message: "Invalid status type!!" + status,
+        });
+      }
+
+      //If there is an existing connection request
+      const existingConnectionRequest = await ConnectionRequest.findOne({
+        $or: [
+          {
+            fromUserId,
+            toUserId,
+          },
+          {
+            fromUserId: toUserId,
+            toUserId: fromUserId,
+          },
+        ],
+      });
+      if (existingConnectionRequest) {
+        return res.status(400).json({
+          message: "Connection request already sent!!",
+        });
+      }
+
       const connectionRequest = new ConnectionRequest({
         fromUserId,
         toUserId,
-        status
+        status,
       });
       const data = await connectionRequest.save(); //This will save the updated user object in the database
       res.json({
         message: `${req.user.firstName} ${req.user.lastName} sent you a connection request!!`,
         data: data,
-      })
-   }
-   catch(err) {
-        res.status(400).send("Something went wrong!!" + err.message);
-   }
-    
-  });
-  
+      });
+    } catch (err) {
+      res.status(400).send("Something went wrong!!" + err.message);
+    }
+  }
+);
+
 module.exports = requestRouter;
