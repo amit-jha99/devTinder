c06e578f881842ba1d5348a9a6303662a662693d
amit-jha99 <amitjha121299@gmail.com>
2025-05-05 00:16:02 +0530

    Refactor user request and connection retrieval routes for consistency and readability

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,52 +1,60 @@
-const express = require('express');
+const express = require("express");
 const userRouter = express.Router();
-const { userAuth } = require('../middlewares/auth');
-const connectionRequest = require('../models/connectionRequest');
+const { userAuth } = require("../middlewares/auth");
+const connectionRequest = require("../models/connectionRequest");
 
 const USER_SAFE_DATA = "firstName lastName photoUrl age";
 
 //get all the pending connection request for the logged in user
-userRouter.get('/user/requests/received', userAuth,async(req,res)=>{
-    try{
-        const loggedInUser = req.user;
-        const connectionRequests = await connectionRequest.find({
-            toUserId: loggedInUser._id,
-            status:"interested",
-        }).populate("fromUserId",["firstName","lastName","photoUrl","age","gender"]); //This will populate the fromUserId field with the firstName, lastName and photoUrl of the user
-        res.json({
-            message: "All the connection requests",
-            data: connectionRequests,
-        });
-
-    }catch(err){
-        res.status(400).send("Something went wrong!!" + err.message);
-    }
-
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionRequests = await connectionRequest
+      .find({
+        toUserId: loggedInUser._id,
+        status: "interested",
+      })
+      .populate("fromUserId", [
+        "firstName",
+        "lastName",
+        "photoUrl",
+        "age",
+        "gender",
+      ]); //This will populate the fromUserId field with the firstName, lastName and photoUrl of the user
+    res.json({
+      message: "All the connection requests",
+      data: connectionRequests,
+    });
+  } catch (err) {
+    res.status(400).send("Something went wrong!!" + err.message);
+  }
 });
 
-userRouter.get('/user/connections',userAuth,async(req,res)=>{
-    try{
-        const loggedInUser = req.user;
-        const connections = await connectionRequest.find({
-            $or:[
-                {
-                    fromUserId:loggedInUser._id,
-                    status:"accepted",
-                }
-                ,{
-                    toUserId:loggedInUser._id,
-                    status:"accepted",
-                }
-            ]
-        }).populate("fromUserId",USER_SAFE_DATA);
-        res.json({
-            message: "All the connections",
-            data: connections,
-        });
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connections = await connectionRequest
+      .find({
+        $or: [
+          {
+            fromUserId: loggedInUser._id,
+            status: "accepted",
+          },
+          {
+            toUserId: loggedInUser._id,
+            status: "accepted",
+          },
+        ],
+      })
+      .populate("fromUserId", USER_SAFE_DATA);
 
-    }
-    catch(err){
-        res.status(400).send("Something went wrong!!" + err.message);
-    }
-})
+    const data = connections.map((row) => row.fromUserId);
+    res.json({
+      message: "All the connections",
+      data: data,
+    });
+  } catch (err) {
+    res.status(400).send("Something went wrong!!" + err.message);
+  }
+});
 module.exports = userRouter;
