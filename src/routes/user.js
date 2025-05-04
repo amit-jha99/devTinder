7ab31a2f2640cbec6e31f4ae0b09ae5528c26e6e
amit-jha99 <amitjha121299@gmail.com>
2025-05-04 21:16:50 +0530

    Implement endpoint to retrieve pending connection requests for logged-in user

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,4 +1,23 @@
 const express = require('express');
 const userRouter = express.Router();
+const { userAuth } = require('../middlewares/auth');
+const connectionRequest = require('../models/connectionRequest');
 
+//get all the pending connection request for the logged in user
+userRouter.get('/user/requests', userAuth,async(req,res)=>{
+    try{
+        const loggedInUser = req.user;
+        const connectionRequests = await connectionRequest.find({
+            toUserId: loggedInUser._id,
+        })
+        res.json({
+            message: "All the connection requests",
+            data: connectionRequests,
+        });
+
+    }catch(err){
+        res.status(400).send("Something went wrong!!" + err.message);
+    }
+
+});
 module.exports = userRouter;
