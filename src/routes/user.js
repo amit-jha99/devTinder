523401c9590038cf9b8d5d7d46fac7a24e49c1fe
amit-jha99 <amitjha121299@gmail.com>
2025-05-05 13:10:45 +0530

    Rename GET /user/feed endpoint to /feed for consistency

--- src/routes/user.js
+++ src/routes/user.js
@@ -63,7 +63,7 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
   }
 });
 
-userRouter.get("/user/feed",userAuth,async (req,res)=>{
+userRouter.get("/feed",userAuth,async (req,res)=>{
     try{
       // User should see all the user cards except 
       //0. his own card
